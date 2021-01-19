2b5f28dd8d0f9c50e1f9773cd0301ff951d0c6b2
luiz.oku <luiz.oku@dxtcwb010714.local>
2021-01-19 18:06:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const pino = require('express-pino-logger')();
+const cors = require('cors');
+
+const config = require('./config');
+const { videoToken } = require('./tokens');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(pino);
+app.use(cors());
+
+const sendTokenResponse = (token, res) => {
+  res.set('Content-Type', 'application/json');
+  res.send(
+    JSON.stringify({
+      token: token.toJwt(),
+    })
+  );
+};
+
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.get('/video/token', (req, res) => {
+  const { identity } = req.query;
+  const { room } = req.query;
+  const token = videoToken(identity, room, config);
+  sendTokenResponse(token, res);
+});
+app.post('/video/token', (req, res) => {
+  const { identity } = req.body;
+  const { room } = req.body;
+  const token = videoToken(identity, room, config);
+  sendTokenResponse(token, res);
+});
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
